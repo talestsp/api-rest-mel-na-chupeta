afaf30a1482187cbb9aa148390fff6055fa80c6c
Tales Tenorio de Souza Pimentel <talestsp@users.noreply.github.com>
2014-11-24 05:20:44 -0300

    hello.py mais organizado!

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/cade_o_leite_que_tava_aqui', methods=['GET'])
 def hello():
-    return "Hello World!"
+    return "O gato 'comeu'!"
 
 if __name__ == "__main__":
     app.run()
