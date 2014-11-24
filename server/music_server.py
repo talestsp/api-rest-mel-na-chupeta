71dd79bd98bd262d39713d978f19c6d637061715
DiegoPereira <diegolimapereira@gmail.com>
2014-11-24 00:43:54 -0300

    flask exemple

---
+++ server/music_server.py
@@ -0,0 +1,18 @@
+import json
+
+from flask import Flask, make_response, request
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def get_db():
+    #user_id = request.args.get('user_id') aqui é pra pegar argumento da url viss tales
+    resp = make_response() #aqui faz uma resposta chamando uma função dentro do make_response
+    resp.headers['Access-Control-Allow-Origin'] = "*" #sempre deixa isso
+    return 'abc'
+    #return resp
+
+
+if __name__ == '__main__':
+    app.debug = True    
+    app.run(host='0.0.0.0', port=9000)
