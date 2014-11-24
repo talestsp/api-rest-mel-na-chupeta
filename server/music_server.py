a47fd1f2510181c5b45fab7c86563e20db0eea1b
DiegoPereira <diegolimapereira@gmail.com>
2014-11-24 00:53:27 -0300

    views dependences added

--- server/music_server.py
+++ server/music_server.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def get_db():
-    #user_id = request.args.get('user_id') aqui é pra pegar argumento da url viss tales
-    resp = make_response() #aqui faz uma resposta chamando uma função dentro do make_response
+    #user_id = request.args.get('user_id') aqui eh pra pegar argumento da url viss tales
+    resp = make_response() #aqui faz uma resposta chamando uma funcao dentro do make_response
     resp.headers['Access-Control-Allow-Origin'] = "*" #sempre deixa isso
     return 'abc'
     #return resp
@@ -15,4 +15,4 @@ def get_db():
 
 if __name__ == '__main__':
     app.debug = True    
-    app.run(host='0.0.0.0', port=9000)
+    app.run(host='0.0.0.0', port=9090)
